46bf709c225ef0bb50c5af2eea6dc1deaa521649
XTer <xxoy1234@outlook.com>
2024-02-17 01:26:55 +0800

    优化了代码结构

--- inference_backend.py
+++ inference_backend.py
@@ -152,99 +152,6 @@ def change_gpt_weights(gpt_path):
 
 
 
-def load_infer_config(config_path='infer_config.json'):
-    """加载环境配置文件"""
-    with open(config_path, 'r', encoding='utf-8') as f:
-        config = json.load(f)
-    return config
-
-def auto_get_infer_config(character_path):
-    # Initialize variables for file detection
-    ckpt_file_found = None
-    pth_file_found = None
-    wav_file_found = None
-
-    # Iterate through files in character_path to find matching file types
-    for file in os.listdir(character_path):
-        if file.endswith(".ckpt"):
-            ckpt_file_found = os.path.join(character_path, file)
-        elif file.endswith(".pth"):
-            pth_file_found = os.path.join(character_path, file)
-        elif file.endswith(".wav"):
-            wav_file_found = os.path.join(character_path, file)
-
-    # If all required files are found, create the infer_config
-    if ckpt_file_found and pth_file_found and wav_file_found:
-        wav_file_name = os.path.splitext(os.path.basename(wav_file_found))[0]  # Extract the filename without extension
-
-        infer_config = {
-            "ref_wav_path": wav_file_found,
-            "prompt_text": wav_file_name,  # Use the extracted wav file name
-            "prompt_language": "中文",
-            "text_language": "中文",
-            "gpt_path": ckpt_file_found,
-            "sovits_path": pth_file_found
-        }
-
-        infer_config_path = os.path.join(character_path, "infer_config.json")
-        with open(infer_config_path, "w") as f:
-            json.dump(infer_config, f)
-
-        return infer_config_path
-    else:
-        return "Required files not found in character_path directory."
-
-def load_character(character_name):
-    try:
-        # 加载配置
-        config = load_infer_config(f"./trained/{character_name}/infer_config.json")
-        # 尝试从环境变量获取gpt_path，如果未设置，则从配置文件读取
-        gpt_path = config.get("gpt_path")
-        # 尝试从环境变量获取sovits_path，如果未设置，则从配置文件读取
-        sovits_path = config.get("sovits_path")
-    except:
-        try:
-            # 尝试调用auto_get_infer_config
-            auto_get_infer_config(f"./trained/{character_name}/")
-            load_character(character_name)
-            return 
-        except:
-            # 报错
-            raise Exception("找不到模型文件！请把有效模型放置在trained文件夹下。")
-    # 修改权重
-    change_sovits_weights(sovits_path)
-    change_gpt_weights(gpt_path)
-
-def get_deflaut_character_name():
-    import os
-    import json
-
-    character_info_path = "./trained/character_info.json"
-    default_character = None
-
-    if os.path.exists(character_info_path):
-        with open(character_info_path, "r") as f:
-            character_info = json.load(f)
-            default_character = character_info.get("deflaut_character")
-
-    if default_character is None:
-        # List all items in "./trained"
-        all_items = os.listdir("./trained")
-        
-        # Filter out only directories (folders) from all_items
-        trained_folders = [item for item in all_items if os.path.isdir(os.path.join("./trained", item))]
-        
-        # If there are any directories found, set the first one as the default character
-        if trained_folders:
-            default_character = trained_folders[0]
-
-    return default_character
-
-
-character_name = get_deflaut_character_name()
-
-load_character(character_name)
-
 def get_spepc(hps, filename):
     audio = load_audio(filename, int(hps.data.sampling_rate))
     audio = torch.FloatTensor(audio)
@@ -499,7 +406,102 @@ def split(todo_text):
             i_split_head += 1
     return todo_texts
 
+##########################以下开始是我写的代码
 
+
+
+def load_infer_config(config_path='infer_config.json'):
+    """加载环境配置文件"""
+    with open(config_path, 'r', encoding='utf-8') as f:
+        config = json.load(f)
+    return config
+
+def auto_get_infer_config(character_path):
+    # Initialize variables for file detection
+    ckpt_file_found = None
+    pth_file_found = None
+    wav_file_found = None
+
+    # Iterate through files in character_path to find matching file types
+    for file in os.listdir(character_path):
+        if file.endswith(".ckpt"):
+            ckpt_file_found = os.path.join(character_path, file)
+        elif file.endswith(".pth"):
+            pth_file_found = os.path.join(character_path, file)
+        elif file.endswith(".wav"):
+            wav_file_found = os.path.join(character_path, file)
+
+    # If all required files are found, create the infer_config
+    if ckpt_file_found and pth_file_found and wav_file_found:
+        wav_file_name = os.path.splitext(os.path.basename(wav_file_found))[0]  # Extract the filename without extension
+
+        infer_config = {
+            "ref_wav_path": wav_file_found,
+            "prompt_text": wav_file_name,  # Use the extracted wav file name
+            "prompt_language": "中文",
+            "text_language": "中文",
+            "gpt_path": ckpt_file_found,
+            "sovits_path": pth_file_found
+        }
+
+        infer_config_path = os.path.join(character_path, "infer_config.json")
+        with open(infer_config_path, "w") as f:
+            json.dump(infer_config, f)
+
+        return infer_config_path
+    else:
+        return "Required files not found in character_path directory."
+
+def load_character(character_name):
+    try:
+        # 加载配置
+        config = load_infer_config(f"./trained/{character_name}/infer_config.json")
+        # 尝试从环境变量获取gpt_path，如果未设置，则从配置文件读取
+        gpt_path = config.get("gpt_path")
+        # 尝试从环境变量获取sovits_path，如果未设置，则从配置文件读取
+        sovits_path = config.get("sovits_path")
+    except:
+        try:
+            # 尝试调用auto_get_infer_config
+            auto_get_infer_config(f"./trained/{character_name}/")
+            load_character(character_name)
+            return 
+        except:
+            # 报错
+            raise Exception("找不到模型文件！请把有效模型放置在trained文件夹下。")
+    # 修改权重
+    change_sovits_weights(sovits_path)
+    change_gpt_weights(gpt_path)
+
+def get_deflaut_character_name():
+    import os
+    import json
+
+    character_info_path = "./trained/character_info.json"
+    default_character = None
+
+    if os.path.exists(character_info_path):
+        with open(character_info_path, "r") as f:
+            character_info = json.load(f)
+            default_character = character_info.get("deflaut_character")
+
+    if default_character is None:
+        # List all items in "./trained"
+        all_items = os.listdir("./trained")
+        
+        # Filter out only directories (folders) from all_items
+        trained_folders = [item for item in all_items if os.path.isdir(os.path.join("./trained", item))]
+        
+        # If there are any directories found, set the first one as the default character
+        if trained_folders:
+            default_character = trained_folders[0]
+
+    return default_character
+
+
+character_name = get_deflaut_character_name()
+
+load_character(character_name)
 
 
 def auto_cut(inp):
@@ -555,44 +557,58 @@ def get_tts_wav_api(text):
     # 注意：这里假设get_tts_wav函数及其所需的其它依赖已经定义并可用
     return get_tts_wav(ref_wav_path, prompt_text, prompt_language, text, text_language)
 
-import soundfile as sf
-from flask import Flask, request, Response, jsonify
-import io
-import urllib.parse
 
-app = Flask(__name__)
+def test_audio_save():
+    fs, audio_to_save=get_tts_wav_api("""大家好我是查特
+花儿不哭大佬开源了一个人工智能语音合成项目
+效果相当不错""")
+    file_path = "./testaudio/example_audio.wav"
+    from scipy.io.wavfile import write
+    write(file_path, fs, audio_to_save)
+
+test_audio_save()
+
+# ####web部分
 
 
-@app.route('/tts', methods=['POST'])
-def tts():
-    global character_name
-    if request.is_json:
-        # 使用 request.json 来访问 JSON 数据
-        data = request.json
+# import soundfile as sf
+# from flask import Flask, request, Response, jsonify
+# import io
+# import urllib.parse
 
-        text = urllib.parse.unquote(data.get('text', ''))
-        cha_name = data.get('cha_name', None)
+# app = Flask(__name__)
 
-        # 构建期望的目录路径
-        expected_path = f"./trained/{cha_name}/" if cha_name else None
 
-        # 检查是否提供了cha_name，且与当前全局变量不同，以及路径是否存在
-        if cha_name and cha_name != character_name and expected_path and os.path.exists(expected_path):
-            character_name = cha_name  # 更新全局变量
-            load_character(character_name)  # 加载新角色
-        elif expected_path and not os.path.exists(expected_path):
-            return jsonify({"error": f"Directory {expected_path} does not exist. Using the current character."}), 400
+# @app.route('/tts', methods=['POST'])
+# def tts():
+#     global character_name
+#     if request.is_json:
+#         # 使用 request.json 来访问 JSON 数据
+#         data = request.json
 
-        sample_rate, audio_data = get_tts_wav_api(text)
+#         text = urllib.parse.unquote(data.get('text', ''))
+#         cha_name = data.get('cha_name', None)
+
+#         # 构建期望的目录路径
+#         expected_path = f"./trained/{cha_name}/" if cha_name else None
+
+#         # 检查是否提供了cha_name，且与当前全局变量不同，以及路径是否存在
+#         if cha_name and cha_name != character_name and expected_path and os.path.exists(expected_path):
+#             character_name = cha_name  # 更新全局变量
+#             load_character(character_name)  # 加载新角色
+#         elif expected_path and not os.path.exists(expected_path):
+#             return jsonify({"error": f"Directory {expected_path} does not exist. Using the current character."}), 400
+
+#         sample_rate, audio_data = get_tts_wav_api(text)
         
-        # 将音频数据转换为二进制流
-        buffer = io.BytesIO()
-        sf.write(buffer, audio_data, sample_rate, format='WAV')
-        buffer.seek(0)
-        return Response(buffer.getvalue(), mimetype='audio/wav')
-    else:
-        return jsonify({"error": "Request must be JSON"}), 400
+#         # 将音频数据转换为二进制流
+#         buffer = io.BytesIO()
+#         sf.write(buffer, audio_data, sample_rate, format='WAV')
+#         buffer.seek(0)
+#         return Response(buffer.getvalue(), mimetype='audio/wav')
+#     else:
+#         return jsonify({"error": "Request must be JSON"}), 400
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=False, host='0.0.0.0')
 
