914898cb8ea62b78f6847115ea672e8c6721eb06
XTer <xxoy1234@outlook.com>
2024-03-01 01:09:25 +0800

    前端可以完全运行在另一台电脑上了

--- src/TTS_Webui.py
+++ src/TTS_Webui.py
@@ -5,38 +5,13 @@ import requests
 import numpy as np
 from string import Template
 
-
-# 取得模型文件夹路径
-global models_path
-config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json")
-
-global characters_and_emotions
-characters_and_emotions = {}
-
-if os.path.exists(config_path):
-    with open(config_path, 'r', encoding='utf-8') as f:
-        config = json.load(f)
-        models_path = config.get("models_path", "trained")
-else:
-    models_path = "trained"
-
-
-def get_character_names():
-    trained_folders = [f for f in os.listdir(models_path) if os.path.isdir(os.path.join(models_path, f))]
-    return trained_folders
-
-def load_info_config(character_name):
+def load_character_emotions(character_name,characters_and_emotions):
     emotion_options = ["default"]
     try:
-        with open(f"{models_path}/{character_name}/infer_config.json", "r", encoding="utf-8") as f:
-            config = json.load(f)
-        emotion_list=config.get('emotion_list', None)
-        if emotion_list is not None:
-            emotion_options = []
-            for emotion, details in emotion_list.items():
-                emotion_options.append(emotion)
+        emotion_options = characters_and_emotions.get(character_name, ["default"])
     except:
         pass
+
             
     return gr.Dropdown(emotion_options, value="default")
 
@@ -77,19 +52,31 @@ def send_request(endpoint, endpoint_data, text, cha_name, text_language, top_k, 
         print(f"请求失败，状态码：{response.status_code}")
 
 
-def change_model_folder(model_folder):
-    global models_path
-    character_names=[]
+def get_characters_and_emotions(character_list_url):
     try:
-        models_path = model_folder
-        character_names = get_character_names()
-        return gr.Dropdown(character_names,value=character_names[0]), gr.Dropdown(["default"], value="default")
+        response = requests.get(character_list_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"请求失败，状态码：{response.status_code}")
+    except:
+        raise Exception("请求失败，请检查URL是否正确")
+
+def change_character_list(character_list_url):
+    
+    characters_and_emotions={}
+    try:
+        characters_and_emotions = get_characters_and_emotions(character_list_url)
+        character_names = [i for i in characters_and_emotions]
+        return gr.Dropdown(character_names,value=character_names[0],label="选择角色"), gr.Dropdown(["default"], value="default", label="情感列表"),gr.State(characters_and_emotions)
 
     except:
-        return gr.Dropdown([],value="", label="选择角色"), gr.Dropdown(["default"], value="default")
-        
+        return gr.Dropdown([],value="", label="选择角色"), gr.Dropdown(["default"], value="default", label="情感列表"),gr.State({})
 
+def change_endpoint(endpoint):
+    return gr.Textbox(endpoint.rsplit('/', 1)[0] + "/character_list")
 
+default_character_info_url = "http://127.0.0.1:5000/character_list"
 default_endpoint = "http://127.0.0.1:5000/tts"
 default_endpoint_data = """{
     "method": "POST",
@@ -106,18 +93,21 @@ default_endpoint_data = """{
 """
 default_text="我是一个粉刷匠，粉刷本领强。我要把那新房子，刷得更漂亮。刷了房顶又刷墙，刷子像飞一样。哎呀我的小鼻子，变呀变了样。"
 
+
+characters_and_emotions = gr.State({})
+
 with gr.Blocks() as app:
     with gr.Row():
         text = gr.Textbox(value=default_text, label="输入文本",interactive=True,lines=8)
     with gr.Row():
         with gr.Column(scale=2):
-            model_folder = gr.Textbox(value=models_path, label="模型文件夹路径（注意不和后端同步，请确保后端支持）")
+            character_list_url = gr.Textbox(value=default_character_info_url, label="人物情感列表网址（改右侧的Endpoint会相应的变化）",interactive=False)
+            
             text_language = gr.Dropdown(["多语种混合", "中文", "英文","日文","中英混合","中日混合"], value="多语种混合", label="文本语言")
-            character_names=get_character_names()
-            cha_name = gr.Dropdown(character_names,value=character_names[0], label="选择角色")
-            character_emotion = gr.Dropdown(["default"], value="default", label="情感列表",interactive=True)  
-            cha_name.change(load_info_config, inputs=[cha_name],outputs=[character_emotion])
-            model_folder.input(change_model_folder, inputs=[model_folder],outputs=[cha_name, character_emotion])
+            cha_name , character_emotion, characters_and_emotions = change_character_list(default_character_info_url)
+            cha_name.change(load_character_emotions, inputs=[cha_name,characters_and_emotions],outputs=[character_emotion])
+            character_list_url.change(change_character_list, inputs=[character_list_url],outputs=[cha_name, character_emotion,characters_and_emotions])
+
         with gr.Column(scale=1):    
             top_k = gr.Slider(minimum=1, maximum=30, value=6, label="Top K",step=1)
             top_p = gr.Slider(minimum=0, maximum=1, value=0.8, label="Top P")
@@ -125,9 +115,11 @@ with gr.Blocks() as app:
         with gr.Column(scale=2):
             endpoint = gr.Textbox(value=default_endpoint, label="Endpoint")
             endpoint_data = gr.Textbox(value=default_endpoint_data, label="发送json格式")
+            endpoint.blur(change_endpoint, inputs=[endpoint],outputs=[character_list_url])
     with gr.Row():
         sendData = gr.Button("发送请求",variant="primary")
         audioRecieve = gr.Audio(None, label="音频输出",type="filepath")
         sendData.click(send_request, inputs=[endpoint, endpoint_data, text, cha_name, text_language, top_k, top_p, temperature, character_emotion], outputs=[audioRecieve])
+
 
 app.launch(server_port=9867, show_error=True)
